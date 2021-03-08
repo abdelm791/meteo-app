4c2ffa80e4fffa30cc5f47a07b49c2e334c76398
abdelm791 <62547166+abdelm791@users.noreply.github.com>
2021-03-08 18:16:55 +0100

    The goal of this project is to create a weather web application using the Angular 9 Framework, this application will allow us to display metrological conditions such as temperature, pressure, weather condition, etc., as well as weather forecasts. , this meteorological data comes from an API (Application Programming Interface) of openweathermap, you just have to look for the city and it will display the data

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { StoreModule } from '@ngrx/store';
+import { locationReducer } from './location-reducer';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherService } from './weather.service';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    TopBarComponent,
+    CurrentWeatherComponent,
+    ForecastComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    StoreModule.forRoot({
+      loc: locationReducer
+    }),
+    FormsModule,
+    MatInputModule,
+    MatTabsModule,
+    MatCardModule,
+    HttpClientModule,
+    MatDividerModule,
+    MatListModule
+  ],
+  providers: [
+    WeatherService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
